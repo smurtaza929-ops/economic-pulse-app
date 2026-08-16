549adfb161413c164ec444d1cd44d028d8b4cb8e
smurtaza929-ops <sam1188@stern.nyu.edu>
2026-08-15 20:03:11 -0400

    flask and project setup branch updated

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,20 +1,15 @@
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
-from web_app.routes.rps_routes import rps_routes
-from web_app.routes.stocks_routes import stocks_routes
-#from web_app.routes.book_routes import book_routes
-#from web_app.routes.weather_routes import weather_routes
+from web_app.routes.economic_routes import economic_routes
+
 
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(home_routes)
-    app.register_blueprint(rps_routes)
-    app.register_blueprint(stocks_routes)
-    #app.register_blueprint(book_routes)
-    #app.register_blueprint(weather_routes)
+    app.register_blueprint(economic_routes)
     return app
 
 if __name__ == "__main__":
